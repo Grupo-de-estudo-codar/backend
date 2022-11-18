1624ca4168ab5cac3a07706a960fbdb6d6d7f5a6
Paulo Ferreira <phgferreira@outlook.com>
2022-11-18 13:34:12 -0300

    Construído script para criação das tabelas cliente, contato, endereco

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+create table cliente (
+    id int primary key auto_increment,
+    nome varchar(255) not null,
+    cpf varchar(11),
+    dataNascimento date
+);
+
+create table contato (
+    id int  primary key auto_increment,
+    tipo_contato varchar(20) not null,
+    codigo varchar(255) not null,
+    id_cliente int not null,
+        foreign key (id_cliente) references cliente(id)
+);
+
+create table endereco (
+    id int primary key auto_increment,
+    logradouro varchar(255) not null,
+    numero varchar(20) not null,
+    complemento clob,
+    cep varchar(8),
+    bairro varchar(100) not null,
+    cidade varchar(100) not null,
+    uf varchar(2) not null,
+    id_cliente int not null,
+        foreign key (id_cliente) references cliente(id)
+);
