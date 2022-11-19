9a265a1e18490b6d51f43ac27b8ee35bd41b63cf
Paulo Ferreira <phgferreira@outlook.com>
2022-11-19 19:34:22 -0300

    Adicionado resposta caso a data de nascimento recebida esteja em um formato incorreto

---
+++ src/main/java/br/com/codarmaismais/backend/exception/advice/DateTimeParseExceptionAdvice.java
@@ -0,0 +1,21 @@
+package br.com.codarmaismais.backend.exception.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.format.DateTimeParseException;
+
+@RestControllerAdvice
+public class DateTimeParseExceptionAdvice {
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DateTimeParseException.class)
+    public String handle(DateTimeParseException exception) {
+        String mensagem = exception.getParsedString() + " não respeita o formato esperado (yyyy-MM-dd)";
+        return mensagem;
+    }
+
+}
