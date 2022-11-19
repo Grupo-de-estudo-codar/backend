f91f1051395436b8d053fc01c4c58bdfa92d52dd
Paulo Ferreira <phgferreira@outlook.com>
2022-11-19 11:11:03 -0300

    Movido a ação de salvar um cliente para dentro do beforeEach

--- src/test/java/br/com/codarmaismais/backend/repository/ClienteRepositoryTest.java
+++ src/test/java/br/com/codarmaismais/backend/repository/ClienteRepositoryTest.java
@@ -1,6 +1,7 @@
 package br.com.codarmaismais.backend.repository;
 
 import br.com.codarmaismais.backend.model.Cliente;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,60 +18,41 @@ class ClienteRepositoryTest {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    Cliente cliente = new Cliente("Fulano de Tal");
+
+    @BeforeEach
+    public void beforeEach() {
+        clienteRepository.save(cliente);
+    }
+
     @Test @DisplayName("Deve guardar um cliente no BD")
     public void test1() {
-        Cliente cliente = new Cliente("Fulano de Tal");
-        clienteRepository.save(cliente);
-
         assertNotNull( cliente.getId() );
     }
 
     @Test @DisplayName("Deve alterar o nome do cliente no BD")
     public void test2() {
-        Cliente cliente = new Cliente("Fulano de Tal");
-        clienteRepository.save(cliente);
         cliente.setNome("José Almeida");
-
         Cliente cliente2 = clienteRepository.getReferenceById( cliente.getId() );
-
         assertEquals("José Almeida", cliente2.getNome());
     }
 
     @Test @DisplayName("Deve deletar um cliente do BD")
     public void test3() {
-        Cliente cliente = new Cliente("Fulano de Tal");
-        clienteRepository.save(cliente);
-
-        Integer idCliente = cliente.getId();
-        clienteRepository.delete(cliente);
-
-        Optional<Cliente> clienteOpcional = clienteRepository.findById(idCliente);
+        clienteRepository.delete( cliente );
+        Optional<Cliente> clienteOpcional = clienteRepository.findById( cliente.getId() );
         assertFalse( clienteOpcional.isPresent() );
     }
 
     @Test @DisplayName("Deve fazer uma busca de cliente por ID no BD")
     public void test4() {
-        Cliente cliente = new Cliente("Fulano de Tal");
-        clienteRepository.save(cliente);
-
         Optional<Cliente> clienteOpcional = clienteRepository.findById( cliente.getId() );
-
         assertTrue( clienteOpcional.isPresent() );
     }
 
     @Test @DisplayName("Deve listar mais de um cliente no BD")
     public void test5() {
-        Cliente cliente = new Cliente("Fulano de Tal");
-        Cliente cliente2 = new Cliente("Jose Maria");
-        Cliente cliente3 = new Cliente("Rafael Aldo");
-
-
-        clienteRepository.save(cliente);
-        clienteRepository.save(cliente2);
-        clienteRepository.save(cliente3);
-
         List<Cliente> clientes = clienteRepository.findAll();
-
         assertNotNull( clientes );
         assertTrue(clientes.size() > 0);
     }
