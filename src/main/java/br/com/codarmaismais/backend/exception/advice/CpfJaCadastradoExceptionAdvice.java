bde372e348ced2d189efe61da2ba8f95b7ceb15c
Paulo Ferreira <phgferreira@outlook.com>
2022-11-21 23:10:35 -0300

    Corrigindo a resposta do CpfJaCadastradoExceptionAdvice

--- src/main/java/br/com/codarmaismais/backend/exception/advice/CpfJaCadastradoExceptionAdvice.java
+++ src/main/java/br/com/codarmaismais/backend/exception/advice/CpfJaCadastradoExceptionAdvice.java
@@ -2,17 +2,18 @@ package br.com.codarmaismais.backend.exception.advice;
 
 import br.com.codarmaismais.backend.exception.CpfJaCadastradoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class CpfCadastradoExceptionAdvice {
+public class CpfJaCadastradoExceptionAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(CpfJaCadastradoException.class)
-    public String hanble(CpfJaCadastradoException exception) {
-        return exception.getMessage();
+    public ResponseEntity hanble(CpfJaCadastradoException exception) {
+        return ResponseEntity.badRequest().body( exception.getMessage() );
     }
 
 }
